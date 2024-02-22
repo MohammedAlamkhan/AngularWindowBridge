2f3413d3fc3e6e302f60da7e3379d0714d52cb07
MohammedAlamKhan <mohammedalamkhan6@gmail.com>
2024-02-22 10:53:53 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {BridgeGetAppsResponse, BridgeInstalledAppInfo} from '@bridgelauncher/api'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +8,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
+  async ngOnInit(): Promise<void> {
+    Bridge.showToast('Hello, world!');
+
+    fetch(Bridge.getAppsURL())
+    .then(resp => resp.json() as unknown as BridgeGetAppsResponse)
+    .then(resp => {
+        // do something with the list of apps
+        resp.apps 
+        Bridge.showToast(JSON.stringify(resp.apps));
+    })
+  }
+
+
   title = 'angularWindowBridge';
 }
