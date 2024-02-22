a3b36671688fccf87aeadc645ff308bbac04fa23
MohammedAlamKhan <mohammedalamkhan6@gmail.com>
2024-02-22 11:11:21 +0530

    implemented app list and launch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {BridgeGetAppsResponse, BridgeInstalledAppInfo} from '@bridgelauncher/api'
+import {CommonModule} from "@angular/common"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
 export class AppComponent implements OnInit{
+  applist: any;
   async ngOnInit(): Promise<void> {
     Bridge.showToast('Hello, world!');
 
@@ -18,11 +20,18 @@ export class AppComponent implements OnInit{
     .then(resp => resp.json() as unknown as BridgeGetAppsResponse)
     .then(resp => {
         // do something with the list of apps
-        resp.apps 
+        this.applist= resp.apps 
         Bridge.showToast(JSON.stringify(resp.apps));
     })
   }
 
+  launchApp(packageName: string){
+    Bridge.requestLaunchApp(packageName)
+  }
+
+  getIcon(packageName: string){
+    Bridge.getDefaultAppIconURL(packageName)
+  }
 
   title = 'angularWindowBridge';
 }
